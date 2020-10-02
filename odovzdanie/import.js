6aea793925ca2f81c082fd618504858cbc54bf4f
Sandro Rybárik <sandro.rybarik@gmail.com>
2020-10-02 20:11:52 +0200

    refactored await

--- odovzdanie/import.js
+++ odovzdanie/import.js
@@ -33,40 +33,36 @@ async function executeBatchAndEmpty(client, qvb) {
     qh_countries, qt_countries, countries
   } = qvb
 
-  const promises = []
-
   if (tweets.length > 0) {
-    promises.push(client.query(
+    await client.query(
       qh_tweets + " " + tweets.join(", ") + " " + qt_tweets
-    ))
+    )
   }
   if (accounts.length > 0) {
-    promises.push(client.query(
+    await client.query(
       qh_accounts + " " + accounts.join(", ") + " " + qt_accounts
-    ))
+    )
   }
   if (tweet_mentions.length > 0) {
-    promises.push(client.query(
+    await client.query(
       qh_tweet_mentions + " " + tweet_mentions.join(", ") + " " + qt_tweet_mentions
-    ))
+    )
   }
   if (tweet_hashtags.length > 0) {
-    promises.push(client.query(
+    await client.query(
       qh_tweet_hashtags + " " + tweet_hashtags.join(", ") + " " + qt_tweet_hashtags
-    ))
+    )
   }
   if (hashtags.length > 0) {
-    promises.push(client.query(
+    await client.query(
       qh_hashtags + " " + hashtags.join(", ") + " " + qt_hashtags
-    ))
+    )
   }
   if (countries.length > 0) {
-    promises.push(client.query(
+    await client.query(
       qh_countries + " " + countries.join(", ") + " " + qt_countries
-    ))
+    )
   }
-
-  await Promise.allSettled(promises)
 
   // resets all to []
   qvb.tweets = []
